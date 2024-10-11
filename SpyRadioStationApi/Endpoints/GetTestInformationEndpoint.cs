6093c36e4f6a61940588e5e14d24dfd010326616
Yury Kosachou <y.kosachou@sam-solutions.com>
2024-10-11 16:53:43 +0300

    create db from reaquests

--- SpyRadioStationApi/Endpoints/GetTestInformationEndpoint.cs
+++ SpyRadioStationApi/Endpoints/GetTestInformationEndpoint.cs
@@ -4,6 +4,7 @@ using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Options;
 using SpyRadioStationApi.Configurations;
 using SpyRadioStationApi.Contracts.Response;
+using SpyRadioStationApi.Interfaces.db;
 using SpyRadioStationApi.Interfaces.Repositories;
 using SpyRadioStationApi.Interfaces.Services;
 using System.Runtime.InteropServices;
@@ -14,16 +15,19 @@ namespace SpyRadioStationApi.Endpoints
     public record HeaderInformation(IHeaderDictionary header, string ip);
     public class GetTestInformationEndpoint : EndpointWithoutRequest<InformationResponse>
     {
+        private readonly IDatabaseBootstrap _bootstrap;
         private readonly DbConfiguration _dbConfiguration;
         private readonly Telegram _telegram;
         private readonly Access _access;
         public GetTestInformationEndpoint(IOptions<DbConfiguration> dbConfiguration,
             IOptions<Telegram> telegram,
-            IOptions<Access> access)
+            IOptions<Access> access,
+            IDatabaseBootstrap bootstrap)
         {
             _dbConfiguration = dbConfiguration?.Value;
             _telegram = telegram?.Value;
             _access = access?.Value;
+            _bootstrap = bootstrap;
         }
         public override void Configure()
         {
@@ -33,6 +37,8 @@ namespace SpyRadioStationApi.Endpoints
 
         public override async Task HandleAsync(CancellationToken ct)
         {
+            await _bootstrap.Setup();
+
             using var connection = new SqliteConnection(_dbConfiguration.DatabaseName);
             var rows = (await connection.QueryAsync<dynamic>("SELECT Name FROM Migrations"))?.Select(x=>(string)x.Name).ToList();
             var information = await connection.QueryAsync<dynamic>("SELECT name FROM sqlite_master");//)?.Select(x => (string)x.Name).ToList();
@@ -44,6 +50,7 @@ namespace SpyRadioStationApi.Endpoints
                 var list = Directory.GetFiles("diff");
                 files = string.Join(",", list);
             }
+
 
             var headers = HttpContext.Request.Headers;
             HttpContext.Request.Headers.TryGetValue("True-Client-Ip", out var remoteIp);
